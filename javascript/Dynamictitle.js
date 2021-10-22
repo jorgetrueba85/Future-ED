43e4d0898238f4bef95310debe770c5a78d14ece
Jorge Trueba <70460144+jorgetrueba85@users.noreply.github.com>
2021-10-22 16:54:10 -0500

    added automatic theme change

--- javascript/Dynamictitle.js
+++ javascript/Dynamictitle.js
@@ -24,3 +24,25 @@ var origTitle = document.title;
  // bind functions to blur and focus events
  window.onblur = newTitle;
  window.onfocus = oldTitle;
+
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
+}
+        if (window.matchMedia) {
+  // Check if the dark-mode Media-Query matches
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    changeCSS('css/stylesdark.css', 0);
+  } else {
+    changeCSS('css/styleslight.css', 0);
+  }
+} else {
+  changeCSS('css/stylesdark.css', 0);
+}
