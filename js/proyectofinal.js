284085da9516aac4cd6611f36c27359f7e644283
Jorge Trueba <j.ed.tru.ga2@gmail.com>
2021-11-10 09:08:57 -0600

    Added Theme selection & caching

--- js/proyectofinal.js
+++ js/proyectofinal.js
@@ -1,3 +1,66 @@
 function logo(){
     location.href='index.html';
 }
+
+
+function orientacion(){
+if (window.matchMedia("(orientation: portrait)").matches) {
+    document.getElementById('cuerpo').innerHTML = '<p>hola</p>';
+ }
+ 
+ if (window.matchMedia("(orientation: landscape)").matches) {
+
+ }
+
+
+ var state=localStorage.getItem("Theme");
+ var root = document.querySelector(':root');
+ var rootStyles = getComputedStyle(root);
+
+ if (state == 'dark'){
+     root.style.setProperty('--navbar', 'rgb(43,43,43)');
+     root.style.setProperty('--icons', 'white');
+     root.style.setProperty('--bg','rgb(1, 1, 82)');
+     root.style.setProperty('--map','rgb(88,88,88)');
+     root.style.setProperty('--maphover','rgb(15, 52, 94)');
+     localStorage.setItem("Tema",'1');
+     localStorage.setItem("Theme",'dark');
+ }
+
+ else{
+     root.style.setProperty('--navbar', 'rgb(255,255,255)');
+     root.style.setProperty('--icons', 'black');
+     root.style.setProperty('--bg','rgb(18, 63, 114)');
+     root.style.setProperty('--map','grey');
+     root.style.setProperty('--maphover','rgb(15, 52, 94)');
+     localStorage.setItem("Tema",'0');
+     localStorage.setItem("Theme",'light');
+ }
+
+}
+
+function temas(){
+    var state=localStorage.getItem("Tema");
+    var root = document.querySelector(':root');
+    var rootStyles = getComputedStyle(root);
+
+    if (state == 0){
+        root.style.setProperty('--navbar', 'rgb(43,43,43)');
+        root.style.setProperty('--icons', 'white');
+        root.style.setProperty('--bg','rgb(1, 1, 82)');
+        root.style.setProperty('--map','rgb(88,88,88)');
+        root.style.setProperty('--maphover','rgb(15, 52, 94)');
+        localStorage.setItem("Tema",'1');
+        localStorage.setItem("Theme",'dark');
+    }
+
+    else{
+        root.style.setProperty('--navbar', 'rgb(255,255,255)');
+        root.style.setProperty('--icons', 'black');
+        root.style.setProperty('--bg','rgb(18, 63, 114)');
+        root.style.setProperty('--map','grey');
+        root.style.setProperty('--maphover','rgb(15, 52, 94)');
+        localStorage.setItem("Tema",'0');
+        localStorage.setItem("Theme",'light');
+    }
+}
