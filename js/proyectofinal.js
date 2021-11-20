86ad95cd6a4f8996f413d0df97f6f7fbcdaab55f
Jorge Trueba <j.ed.tru.ga2@gmail.com>
2021-11-19 19:50:46 -0600

    Added margin & finishing touches

--- js/proyectofinal.js
+++ js/proyectofinal.js
@@ -86,11 +86,12 @@ function modo(modo){
     switch(modo){
     case 'sun':
         document.getElementById("modal").style.display = "block";
+        document.getElementById("state").innerHTML = estadou;
         document.getElementById("forecast").innerHTML = desc;
-        document.getElementById("temp").innerHTML = temp;
-        document.getElementById("humidity").innerHTML = humidity;
-        document.getElementById("pressure").innerHTML = pressure;
-        document.getElementById("sensation").innerHTML = sensation;
+        document.getElementById("temp").innerHTML = temp + " Cº";
+        document.getElementById("humidity").innerHTML = "Humedad: " + humidity + "%";
+        document.getElementById("pressure").innerHTML = "Presión Atmosférica: " + pressure + "hPA";
+        document.getElementById("sensation").innerHTML = "Sensación Térmica: " + sensation + " Cº";
         document.getElementById("sun").classList.add('selectorbuttonfocus');
         document.getElementById("wind").classList.remove('selectorbuttonfocus');
         document.getElementById("extra").classList.remove('selectorbuttonfocus');
@@ -98,10 +99,12 @@ function modo(modo){
 
     case 'wind':
         document.getElementById("modal").style.display = "block";
-        document.getElementById("forecast").innerHTML = wndspd + ' m/s';
-        document.getElementById("temp").innerHTML = wnddeg + 'º';
+        document.getElementById("state").innerHTML = estadou;
+        document.getElementById("forecast").innerHTML = "Velocidad: " + wndspd + ' m/s';
+        document.getElementById("temp").innerHTML = "Aceleración: " + wndgust + "";
+        document.getElementById("sensation").innerHTML = "Hacia: " + wnddeg + 'º';
         document.getElementById("humidity").innerHTML = '';
-        document.getElementById("pressure").innerHTML = '';
+        document.getElementById("pressure").innerHTML = "Presión Atmosférica: " + pressure + "hPA";
         document.getElementById("sun").classList.remove('selectorbuttonfocus');
         document.getElementById("wind").classList.add('selectorbuttonfocus');
         document.getElementById("extra").classList.remove('selectorbuttonfocus');
@@ -134,120 +137,157 @@ var wndspd;
 var sensation;
 var pressure;
 var humidity;
-
+var estadou;
 
 function buscar(estado){
+estadou = estado;
    switch(estado){
        case "Baja California":
            lat = 32.61;
            lon = -115.52;
+           modo('sun');
            break;
 
         case "Baja California Sur":
             lat = 24.11;
             lon = -110.37;
+            modo('sun');
            break;
 
         case "Coahuila":
             lat = 25.43;
             lon = -101.05;
+            modo('sun');
            break;
 
         case "Chihuahua":
             lat = 28.67;
             lon = -106.20;
+            modo('sun');
            break;
 
         case "Durango":
             lat = 24.02;
             lon = -104.71;
+            modo('sun');
            break;
 
         case "Sinaloa":
             lat = 24.80;
             lon = -107.49;
+            modo('sun');
            break;
 
         case "Sonora":
+            modo('sun');
            break;
 
         case "Zacatecas":
+            modo('sun');
            break;
 
         case "Nuevo León":
+            modo('sun');
            break;
 
         case "San Luis Potosí":
+            modo('sun');
             break;
  
         case "Tamaulipas":
+            modo('sun');
             break;
  
         case "Aguascalientes":
+            modo('sun');
             break;
  
         case "Colima":
+            modo('sun');
             break;
  
         case "Jalisco":
+            modo('sun');
             break;
  
         case "Michoacán":
+            modo('sun');
             break;
  
         case "Nayarit":
+            modo('sun');
             break;
  
         case "Campeche":
+            modo('sun');
             break;
  
         case "Oaxaca":
+            modo('sun');
             break;
 
         case "Puebla":
+            modo('sun');
            break;
 
         case "Tabasco":
+            modo('sun');
            break;
 
         case "Tlaxcala":
+            modo('sun');
            break;
 
         case "Distrito Federal":
+            modo('sun');
            break;
 
         case "Guanajuato":
+            modo('sun');
            break;
 
         case "Guerrero":
+            modo('sun');
            break;
 
         case "Hidalgo":
+            modo('sun');
            break;
 
         case "México":
+            modo('sun');
            break;
 
         case "Morelos":
+            modo('sun');
            break;
 
         case "Querétaro":
+            modo('sun');
            break;
 
         case "Veracruz":
+            modo('sun');
            break;
 
         case "Chiapas":
+            modo('sun');
            break;
 
         case "Quintana Roo":
+            modo('sun');
            break;
 
         case "Yucatán":
             lat = 20.98;
             lon = 89.77;
+            modo('sun');
            break;
 
+        case "Test":
+            lat = 20.98;
+            lon = 89.77;
+            break;
         default:
             break;
    }
